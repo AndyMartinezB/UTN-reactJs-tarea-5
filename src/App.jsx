494298fbbb905713e7ec7aa4d65447260467f8b8
Andy Martinez Bologna <andres.bologna@mi.unc.edu.ar>
2025-12-26 12:57:49 -0300

    feat: test de hooks con ejemplo de contador

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function useLocalStorage(key, initialValue) {
+  const [storedValue, setStoreValue] = useState(() => {
+    try {
+      const item = window.localStorage.getItem(key)
+      return item ? JSON.parse(item) : initialValue
+    } catch (error) {
+      console.error("Error al intentar leer localStorage", error)
+      return initialValue
+    }
+  })
 
+  useEffect(() => {
+    try {
+      const valueToStore = typeof storedValue === 'function' ? storedValue(storedValue) : storedValue
+      window.localStorage.setItem(key, JSON.stringify(valueToStore))
+    } catch (error) {
+      console.error("Error al intentar guardar en localStorage", error)
+    }
+  }, [key, storedValue])
+
+  return [storedValue, setStoreValue]
+}
+
+
+function ContadorPersistente() {
+  const [contador, setContador] = useLocalStorage('contador', 0);
+
+  return(
+    <div className="tarjeta-contador">
+      <p className="texto-contador">contador: {contador}</p>
+      <div className="botones-contador">
+        <button onClick={() => setContador(contador +1) }> Incrementar</button>
+        <button onClick={() => setContador(0) }> Resetear</button>
+      </div>
+    </div>
+  )
+}
+
+function App() {
+  
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h2>Test</h2>
+      <ContadorPersistente/>
     </>
   )
 }
